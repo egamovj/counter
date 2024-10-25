dce64c40695bead7e708c627e6b6c8e1f6049539
egamovj <egamovj90@gmail.com>
2024-10-25 20:23:22 +0500

    first vue app

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <h1>Counter: {{ counter }}</h1>
+    <div class="d-flex">
+      <button v-on:click="onIncrement" class="btn btn-success">Add</button>
+      <button v-on:click="onDecrement" class="btn btn-danger">Delete</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      counter: 0,
+    }
+  },
+  methods: {
+    onIncrement() {
+      this.counter += 1
+    },
+    onDecrement() {
+      this.counter -= 1
+    },
+  }
+}
+</script>
+
+<style></style>
